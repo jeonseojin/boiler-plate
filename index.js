28eb2f52d0ab528d37865426aa7bb29fa43b58f1
seojin <lijvlij@naver.com>
2021-01-03 19:43:36 +0900

    비밀번호 암호화( 및 실행 확인 )

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } =require('./models/User');
+const config = require('./config/key');
+
+
+
+
+//body-parser에 옵션주기
+app.use(bodyParser.urlencoded({ extended: true }));//application/x-www-urlencoded를 파악해서 가져옴
+app.use(bodyParser.json());//application/json을 파악해서 가져옴
+
 
 //mongoDB 연결
 const mongoose = require('mongoose')
-mongoose.connect(config.mongoURI, {
+mongoose.connect( config.mongoURI , {
     useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => {  res.send('Hello World!') })
+app.get('/', (req, res) => {  res.send('Hello World! 안녕~') })
+
+app.post('/register', (req, res) => {
+
+
+    
+    // 회원가입할 때 필요한 정보들을 클라이언트에서 자겨오면
+    // 그것들을 데이터베이스에 넣어준다
+
+    // 인스턴스 생성
+    const user = new User(req.body)//req.body 안에 json 형식의 id / password 등이 들어있음
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+            return res.status(200).json({
+                successs: true
+            })
+        })
+    })
 
 app.listen(port, () => {  console.log(`Example app listening at http://localhost:${port}`)})
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;// salt를 이용해서 비밀번호를 암호화하며 saltRouds의 갯수를 참고하여 생성
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -11,7 +14,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 5
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -28,6 +31,25 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+//mongoose에서 가져온 methods
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if (user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+        // index.js에서 user.save를 하기 전에 이루어 지는 코드
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if (err) return next(err)
+            bcrypt.hash(user.password/* 입력받은 비밀번호 */, salt, function(err, hash/* 암호화된 비밀번호 */) {
+                if (err) return next(err)
+                user.password =hash
+                next()
+            })
+        })
+    }
+    
 })
 
 const User = mongoose.model('User', userSchema)
