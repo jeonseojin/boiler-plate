4a92557da719830e340de633d95d1079bb1335ec
seojin <lijvlij@naver.com>
2021-01-11 19:52:08 +0900

    react hooks

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers, conbineReducers } from 'redux';
+import user from './user_reducer';
+
+
+const rootReducer = combineReducers({
+    // user,
+})
+
+export default rootReducer;
