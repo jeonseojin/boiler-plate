12dac15efc0ceb01b51efe5c2361efd67faf5f90
seojin <lijvlij@naver.com>
2021-01-13 11:36:49 +0900

    로그인

--- client/src/_reducers/User_reducer.js
+++ client/src/_reducers/User_reducer.js
@@ -1,2 +1,14 @@
-
 // 로그인 및 레지스터 기능을 만들때 설명
+import {LOGIN_USER, REGISTER_USER} from '../_actions/types';
+
+export default function (state = {}, action){
+    switch (action.type) {
+        case LOGIN_USER:
+                return { ...state, loginSuccess: action.payload }
+            break;
+
+        default:
+            return state;
+    }
+
+}
